33a036ed2a202dc07898cc8a35d8849d42329092
Ricky Johnson <ricky.johnson@techsoft3d.com>
2022-11-07 09:04:32 -0500

    initial commit

---
+++ javascripts/conversionService.js
@@ -0,0 +1,30 @@
+modelUIDs = [
+       "3804b00f-8197-4ac0-be9b-61fef7bbc914" //boeing full
+]
+
+async function startViewer() {
+        const conversionServiceURI = "https://csapi.techsoft3d.com";
+
+        var viewer;
+
+        let res = await fetch(conversionServiceURI + '/api/streamingSession');
+        var data = await res.json();
+
+        await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(modelUIDs) } });
+
+        viewer = new Communicator.WebViewer({
+                containerId: "viewerContainer",
+                endpointUri: 'wss://' + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                model: "boeing-full",
+                // streamingMode: Communicator.StreamingMode.OnDemand,
+                boundingPreviewMode: Communicator.BoundingPreviewMode.All,
+                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@2022.2",
+                rendererType: 0
+        });
+
+        viewer.start();
+
+        return viewer;
+
+}
+
